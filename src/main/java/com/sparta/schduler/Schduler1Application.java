1f415365217beb75b28a9aae0819e047b37b745b
Choi1999 <dprtldk157@gmail.com>
2024-08-16 04:07:00 +0900

    Initial commit

---
+++ src/main/java/com/sparta/schduler/Schduler1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.schduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schduler1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schduler1Application.class, args);
+    }
+
+}
